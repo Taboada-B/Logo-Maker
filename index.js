745d71ebdb650dd6ac23a0a01f62c7a4d3e0f103
Bryan Taboada <b.taboada123@gmail.com>
2024-07-13 13:06:24 -0600

    trouble shooting index.js, added to gitignore, installed inquirer

--- index.js
+++ index.js
@@ -4,50 +4,52 @@ const inquirer = require('inquirer');
 
 // user questions
 const questions = [
-{
-    type: confirm,
-    name: fileType,
-    message: 'SVG',
-},
-{
-    type: list,
-    name: shape,
-    choices: [circle, square, triangle],
-},
-{
-    type: input,
-    name: backgroundColor,
-    message: 'Enter color for background color' ,
-},
-{
-    type: input,
-    name: fontColor,
-    message: 'Enter color for text color',
-},
+    {
+        type: 'confirm',
+        name: 'fileType',
+        message: 'SVG',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        choices: ['circle', 'square', 'triangle'],
+    },
+    {
+        type: 'input',
+        name: 'backgroundColor',
+        message: 'Enter color for background color',
+    },
+    {
+        type: 'input',
+        name: 'fontColor',
+        message: 'Enter color for text color',
+    },
 
 ]
 
-function logo () {
+
+
+function logo() {
     inquirer
-    // asking for user input
-    .prompt(questions)
-    .then(answers => {
-        return render(answers);
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+        .prompt(questions)
 
-
-
-
-};
+        .then((answers) => {
+            console.log('made it here 1');
+            console.log(answers);
+            // const fileName = 'README.md';
+            // writeToFile(fileName, answers);
+            return render(answers);
+        })
+        .catch((error) => {
+            console.error('There was an error:', error);
+        });
+}
 
 function render(answers) {
-console.log('we made it here');
-console.log(answers);
+    console.log('we made it here 2');
+    console.log(answers);
 
-}; 
+};
 
 
 
